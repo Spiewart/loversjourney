6a9f383fa10d04da81685e919a017b954c8573ec
Dave <spiewart@gmail.com>
2024-04-18 07:49:14 -1000

    First loversjourney commit.

---
+++ loversjourney/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from loversjourney.users.models import User
+from loversjourney.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
